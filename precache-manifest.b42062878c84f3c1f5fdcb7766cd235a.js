732b05adf75495662c871f63dc652c059505f8c2
Mr0Strange1pwn <Pawan.Tiwari@mechlintech.com>
2020-02-28 10:51:50 +0530

    Updates

---
+++ precache-manifest.b42062878c84f3c1f5fdcb7766cd235a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf18368e45acae43b57a5673a39ed856",
+    "url": "/dummysite/index.html"
+  },
+  {
+    "revision": "63360888ca2f696aded8",
+    "url": "/dummysite/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a767f169e2418e40f1e9",
+    "url": "/dummysite/static/js/2.c23e6efa.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/dummysite/static/js/2.c23e6efa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63360888ca2f696aded8",
+    "url": "/dummysite/static/js/main.53ab2138.chunk.js"
+  },
+  {
+    "revision": "79c1442f8130a265e8c3",
+    "url": "/dummysite/static/js/runtime-main.b7b4c261.js"
+  }
+]);
